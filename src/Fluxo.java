4c2ea0319bdbd9232386417409faae7a79810f85
Júlio Amorim <julio.l.amorim00@gmail.com>
2021-09-13 23:10:01 -0300

    Just testing

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -2,8 +2,13 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+            metodo1();
+        } catch (ArithmeticException  | NullPointerException ex) {
+            System.out.println(ex.getMessage());
+        }
         System.out.println("Fim do main");
+
     }
 
     private static void metodo1() {
@@ -15,7 +20,9 @@ public class Fluxo {
     private static void metodo2() {
         System.out.println("Ini do metodo2");
         for (int i = 1; i <= 5; i++) {
+
             System.out.println(i);
+            System.out.println(i / 0);
         }
         System.out.println("Fim do metodo2");
     }
